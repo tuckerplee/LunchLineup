af78f1d31d49c1fed59db2f2df255b1c3e4c062a
Tucker Lee <tuckerplee@me.com>
2026-03-06 10:52:42 -0800

    Fix lunch-breaks module auth dependencies

--- apps/api/src/lunch-breaks/lunch-breaks.module.ts
+++ apps/api/src/lunch-breaks/lunch-breaks.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BillingModule } from '../billing/billing.module';
+import { AuthModule } from '../auth/auth.module';
 import { LunchBreaksController } from './lunch-breaks.controller';
 import { LunchBreaksService } from './lunch-breaks.service';
 
 @Module({
-    imports: [BillingModule],
+    imports: [BillingModule, AuthModule],
     controllers: [LunchBreaksController],
     providers: [LunchBreaksService],
 })
 export class LunchBreaksModule { }
-
