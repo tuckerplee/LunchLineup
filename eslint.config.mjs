2bfb8a08d16351d9a838da38604d41c35a5e17b5
Tucker Lee <tuckerplee@me.com>
2026-06-02 17:03:34 -0700

    Fix CI lint setup

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+
+export default [
+  {
+    ignores: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/.next/**',
+      '**/.turbo/**',
+      '**/playwright-report/**',
+      '**/test-results/**',
+      '**/*.tsbuildinfo',
+      'old/**',
+    ],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 'latest',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+];
